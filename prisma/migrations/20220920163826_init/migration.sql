da230c1862d10c5dbf231dbfbb13f84be9fcb938
Alvin Kigen <cartezalvin@gmail.com>
2022-09-20 19:45:19 +0300

    created an upstream branch

--- prisma/migrations/20220920163826_init/migration.sql
+++ prisma/migrations/20220920163826_init/migration.sql
@@ -25,7 +25,6 @@ CREATE TABLE "AnimalType" (
 CREATE TABLE "AnimalCategory" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
     "typeId" INTEGER NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -47,14 +46,56 @@ CREATE TABLE "AnimalBreed" (
 CREATE TABLE "Animal" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "tag" TEXT NOT NULL,
-    "imageUrl" TEXT NOT NULL,
+    "imageUrl" TEXT,
     "breedId" INTEGER NOT NULL,
     "genderId" INTEGER NOT NULL,
     "bornAt" DATETIME NOT NULL,
-    "purchasedAt" DATETIME,
+    "purchasedAt" DATETIME NOT NULL,
     "status" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
     CONSTRAINT "Animal_breedId_fkey" FOREIGN KEY ("breedId") REFERENCES "AnimalBreed" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Animal_genderId_fkey" FOREIGN KEY ("genderId") REFERENCES "Gender" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "email" TEXT NOT NULL,
+    "phoneNo" INTEGER NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "roleId" INTEGER NOT NULL,
+    CONSTRAINT "Users_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "UnitsOfMeasurement" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "unit" TEXT NOT NULL,
+    "initial" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uomId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    CONSTRAINT "Products_uomId_fkey" FOREIGN KEY ("uomId") REFERENCES "UnitsOfMeasurement" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AnimalCategory_name_key" ON "AnimalCategory"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
